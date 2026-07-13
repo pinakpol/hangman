79c52fa3fb5d9a96c93d6655d35b59a48a5f691e
pinakpol <pinakpol@gmail.com>
2026-07-14 00:16:39 +0600

    Update GameManager.js

--- game/GameManager.js
+++ game/GameManager.js
@@ -1,49 +1,95 @@
 const Game = require("./Game");
 
-class GameManager {
-
-    constructor() {
-
+class GameManager
+{
+    constructor()
+    {
         this.games = {};
 
+        this.TIMEOUT = 60 * 60 * 1000; // 1 hour
+
+        setInterval(
+            () => this.cleanup(),
+            60000
+        );
     }
 
-    getGame(boardID) {
+    //------------------------------------------------
 
-        if (!this.games[boardID]) {
+    getGame(boardID)
+    {
+        if(!this.games[boardID])
+        {
+            this.games[boardID] =
+            {
+                game : new Game(),
 
-            this.games[boardID] = new Game();
-
+                lastAccess :
+                Date.now()
+            };
         }
 
-        return this.games[boardID];
+        this.games[boardID].lastAccess =
+            Date.now();
 
+        return this.games[boardID].game;
     }
 
-    start(boardID, category) {
+    //------------------------------------------------
 
-        return this.getGame(boardID).start(category);
-
+    start(boardID,category)
+    {
+        return this.getGame(boardID)
+            .start(category);
     }
 
-    guess(boardID, letter) {
+    //------------------------------------------------
 
-        this.getGame(boardID).guess(letter);
-
+    guess(boardID,letter)
+    {
+        this.getGame(boardID)
+            .guess(letter);
     }
 
-    state(boardID) {
+    //------------------------------------------------
 
-        return this.getGame(boardID).state();
-
+    state(boardID)
+    {
+        return this.getGame(boardID)
+            .state();
     }
 
-    reset(boardID) {
+    //------------------------------------------------
 
-        this.getGame(boardID).reset();
-
+    reset(boardID)
+    {
+        this.getGame(boardID)
+            .reset();
     }
 
+    //------------------------------------------------
+
+    cleanup()
+    {
+        let now = Date.now();
+
+        for(const id in this.games)
+        {
+            if(
+                now -
+                this.games[id].lastAccess >
+                this.TIMEOUT
+            )
+            {
+                delete this.games[id];
+
+                console.log(
+                    "Removed inactive board:",
+                    id
+                );
+            }
+        }
+    }
 }
 
 module.exports = GameManager;
