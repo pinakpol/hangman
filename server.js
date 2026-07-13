2ada5f5942fb9a5b47ea0e45113f38cef4e15a43
pinakpol <pinakpol@gmail.com>
2026-07-13 22:04:03 +0600

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const games = new GameManager();
 
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("Public"));
 
 
 //----------------------------------------
